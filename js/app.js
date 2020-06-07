456ab3a485d4e5d0aa6a4d2adb3bf267dc946b25
Fanny <f.pringere@gmail.com>
2020-06-07 21:57:17 +0200

    premier commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+import Calculator from './calculator.js';
+
+let calc = new Calculator();
+
+let calc2 = new Calculator();
+
+
+
+
+
+// Idea for addEventListener calculator 
+// let class = event.target.className;
+// switch(class) {
+//     case "calcul__operator": 
+//         screen.textContent = event.target.ad
+//         break;
+//     case "calcul__supp":
+//         screen.text = ""
+
+//     case "calcul__total":
+//         eval(screen.textContent)
+// }
+
